f254b2bae93ae8096a6b90f0969cb566399de529
ejmount <gentlemanoctopus@github.fake>
2013-06-17 16:33:15 +0100

    Prototypes, and changed built to .NET 2

--- PubSub/PubSub.cs
+++ PubSub/PubSub.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 
 public static class PubSub
 {
-		
+	public static void Subscribe<T>(string[] topic, Action<T> callback)
+	{
+
+	}
+
+	public static void Broadcast<T>(string[] topic, T payload)
+	{
+
+	}
+	
+	
 }
