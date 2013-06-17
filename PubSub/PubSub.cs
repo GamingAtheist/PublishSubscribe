c3c899f117030e731ec75bb1d3104abb992c5e63
ejmount <gentlemanoctopus@github.fake>
2013-06-17 20:36:04 +0100

    So this became more complicated than I was expecting

--- PubSub/PubSub.cs
+++ PubSub/PubSub.cs
@@ -2,18 +2,60 @@
 using System.Collections.Generic;
 using System.Text;
 
-
-public static class PubSub
+namespace PubSub
 {
-	public static void Subscribe<T>(string[] topic, Action<T> callback)
+
+	internal class TreeNode
 	{
+		Dictionary<string, TreeNode> children = new Dictionary<string, TreeNode>();
+		List<Delegate> payloads = new List<Delegate>();
+		List<Delegate> wildcardListeners = new List<Delegate>();
+
+		public TreeNode GetChild(string key)
+		{
+			if (!children.ContainsKey(key))
+				children.Add(key, new TreeNode());
+			return children[key];
+		}
+
+		public List<Delegate> GetPayloads()
+		{
+			return new List<Delegate>(payloads);
+		}
+
+		public List<Delegate> GetWildcards()
+		{
+			return new List<Delegate>(wildcardListeners);
+		}
+
+		public void AddPayload<T>(Action<T> callback)
+		{
+			payloads.Add(callback);
+		}
+
+		public void AddWildCard<T>(Action<T> callback)
+		{
+			wildcardListeners.Add(callback);
+		}
 
 	}
 
-	public static void Broadcast<T>(string[] topic, T payload)
+
+
+	public static class Transmitter
 	{
+		TreeNode root = new TreeNode();
+
+		public static void Subscribe<T>(string[] topic, Action<T> callback)
+		{
+			
+		}
+
+		public static void Broadcast<T>(string[] topic, T payload)
+		{
+
+		}
+
 
 	}
-	
-	
 }
